a39dd558d5019dbece2200bb39066aefc20aca25
Bruna <brunaadam54@gmail.com>
2017-08-23 21:39:45 -0300

    Ativ1

---
+++ src/Ativ1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Ativ1 {
+public static void main (String[]args){
+	Scanner t = new Scanner(System.in);
+	int A,B;
+	A = t.nextInt();
+	B = t.nextInt();
+	System.out.println("X = " + ( A + B ));
+	
+}
+}
